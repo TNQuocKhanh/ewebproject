eea8e54b650d3435f3d34ea88727097b689e432b
Thai Duong <19110009@student.hcmute.edu.vn>
2023-03-25 15:48:52 +0700

    Update code Header, Slider

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -5,6 +5,9 @@ import {
   AiOutlineShoppingCart,
   AiOutlineUser,
   AiOutlineLogin,
+  AiOutlineBars,
+  AiOutlineCaretDown,
+  AiOutlineLaptop,
 } from "react-icons/ai";
 import cartContext from "../../contexts/cart/cartContext";
 import SearchBar from "./SearchBar";
@@ -14,6 +17,7 @@ import { storage } from "../../utils";
 const Header = () => {
   const { cartItems } = useContext(cartContext);
   const [isSticky, setIsSticky] = useState(false);
+  const [openCategory, setOpenCategory] = useState(false)
 
   const navigate = useNavigate();
 
@@ -49,6 +53,11 @@ const Header = () => {
     setUserProfile(res);
   };
 
+  // Open category
+  const toggleCategory = () => {
+    openCategory === false ? setOpenCategory(true) : setOpenCategory(false)
+  }
+
   useEffect(() => {
     if (storage.load("user")) {
       getUserProfile();
@@ -74,64 +83,101 @@ const Header = () => {
               </button>
             </div>
             <nav className="nav_actions">
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <div className="user_action" style={{ border: '1px solid black', borderRadius: '50%', color: 'var(--main-color-2)', background: 'var(--main-color)' }}>
+                  <span>
+                    <AiOutlineUser style={{ margin: '10px' }} />
+                  </span>
+                  <div className="dropdown_menu">
+                    <ul>
+                      <li>
+                        <Link to="/profile">Thông tin</Link>
+                      </li>
+                      <li>
+                        <Link to="/change-password">Đổi mật khẩu</Link>
+                      </li>
+                      <li>
+                        <Link to="/logout" onClick={handleLogout}>
+                          Đăng xuất
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div style={{ display: 'flex', flexDirection: 'column', padding: '0 8px' }}>
+                  <small style={{ fontSize: '0.7rem', paddingBottom: '2px' }}>Xin chào,</small>
+                  <strong style={{ fontSize: '0.8rem', paddingTop: '2 px' }}>
+                    {userProfile?.fullName} Khanh Sky
+                  </strong>
+                </div>
+              </div>
               <div className="cart_action">
                 <Link to="/cart">
-                  <AiOutlineShoppingCart />
-                  {cartQuantity > 0 && (
-                    <span className="badge">{cartQuantity}</span>
-                  )}
+                  <div style={{ border: '1px solid black', borderRadius: '50%', color: 'var(--main-color)', background: 'black' }}>
+                    <AiOutlineShoppingCart style={{ margin: '10px' }} />
+                    {cartQuantity > 0 && (
+                      <span className="badge">{cartQuantity}</span>
+                    )}
+                  </div>
                 </Link>
                 <div className="tooltip">Giỏ hàng</div>
               </div>
-
-              <div className="user_action">
-                <span>
-                  <AiOutlineUser />
-                </span>
-                <div className="dropdown_menu">
-                  <ul>
-                    <li>
-                      <Link to="/profile">Thông tin</Link>
-                    </li>
-                    <li>
-                      <Link to="/change-password">Đổi mật khẩu</Link>
-                    </li>
-                    <li>
-                      <Link to="/logout" onClick={handleLogout}>
-                        Đăng xuất
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div>{userProfile?.fullName}</div>
-              {!storage.load('user') && <div>
-                <Link to="/login">
-                  <AiOutlineLogin />
-                </Link>
-              </div>}
+              {
+                // !storage.load('user') &&
+                // <div style={{ display: 'flex', alignItems: 'center' }}>
+                //   <Link to="/login">
+                //     <strong style={{ fontSize: '0.9rem' }}>Đăng nhập</strong>
+                //   </Link>
+                //   <small style={{ margin: '0 5px' }}>|</small>
+                //   <Link to="/register">
+                //     <small style={{ fontSize: '0.9rem' }}>Đăng ký </small>
+                //   </Link>
+                // </div>
+              }
             </nav>
           </div>
         </div>
-        <div style={{ marginTop: "15px" }} className="navigation container">
-          <div>Danh mục</div>
+        <div style={{ opacity: 0.2, background: 'gray', height: '1px' }}></div>
+        <div style={{ marginTop: "5px", marginBottom: '5px' }} className="navigation container">
+          <div style={{ position: 'relative' }}>
+            <div onClick={toggleCategory} style={{ display: 'flex', alignItems: 'center', background: '#ffdc8be0', padding: '8px 15px', borderRadius: '5px', cursor: 'pointer' }}>
+              <AiOutlineBars style={{ fontSize: '1.4rem', marginRight: '10px' }} />
+              <strong style={{ fontSize: '0.9rem' }}>Danh mục sản phẩm</strong>
+              <AiOutlineCaretDown style={{ fontSize: '1rem', marginLeft: '20px' }} />
+            </div>
+            <div hidden={openCategory ? false : true} style={{ position: 'absolute', width: '100%', borderRadius: '5px', background: 'white' }}>
+              <ul style={{ borderRadius: '10px' }}>
+                <li className="category-item" style={{ padding: '15px', display: 'flex', alignItems: 'center' }}>
+                  <AiOutlineLaptop style={{fontSize: '20px', marginRight: '10px'}} /> Laptop
+                </li>
+                <li className="category-item" style={{ padding: '15px' }}>
+                  <AiOutlineLaptop style={{fontSize: '20px', marginRight: '10px'}} /> Phone
+                </li>
+                <li className="category-item" style={{ padding: '15px' }}>
+                  <AiOutlineLaptop style={{fontSize: '20px', marginRight: '10px'}} /> Tablet
+                </li>
+                <li className="category-item" style={{ padding: '15px' }}>
+                  <AiOutlineLaptop style={{fontSize: '20px', marginRight: '10px'}} /> Gaming
+                </li>
+              </ul>
+            </div>
+          </div>
           <div>
-            <NavLink style={{ marginLeft: "10px" }} to="/">
-              Trang chủ
+            <NavLink style={{ marginLeft: "15px", fontSize: '0.8rem' }} to="/">
+              <strong>Trang chủ</strong>
             </NavLink>
-            <NavLink style={{ marginLeft: "10px" }} to="/cart">
-              Giỏ hàng
+            <NavLink style={{ marginLeft: "15px", fontSize: '0.8rem' }} to="/cart">
+              <strong>Giỏ hàng</strong>
             </NavLink>
-            <NavLink style={{ marginLeft: "10px" }} to="/all-products">
-              Sản phẩm
+            <NavLink style={{ marginLeft: "15px", fontSize: '0.8rem' }} to="/all-products">
+              <strong>Sản phẩm</strong>
             </NavLink>
-            <NavLink style={{ marginLeft: "10px" }} to="/orders">
-              Đơn hàng
+            <NavLink style={{ marginLeft: "15px", fontSize: '0.8rem' }} to="/orders">
+              <strong>Đơn hàng</strong>
             </NavLink>
           </div>
         </div>
       </header>
-
       <SearchBar />
     </>
   );
